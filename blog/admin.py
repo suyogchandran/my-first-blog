e59108008631c24c5734e6097e1d5a1deb319eb8
suyogchandran <chandransuyog@gmail.com>
2019-04-16 11:23:03 +0530

    modified the admin page with id and author

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author')
